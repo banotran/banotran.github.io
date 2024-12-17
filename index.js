025267266621d39d5ee4681a9746e18b4a36d353
bano <txbac1998@gmail.com>
2024-12-17 12:09:11 +0700

    king cat studio

--- index.js
+++ index.js
@@ -1,72 +1,89 @@
 let data = [
-    // {
-    //     id: 0, title: "Jump into the Sky (Mavis)", description: 
-    //     "Jump, parkour, and earn Ronin tokens in the sky – it's all free!",
-    //     image: "./crossads/images/com.onlywayisup.climbparkour.png",
-    //     imageIcon: "./imageWeb/website.png",
-    //     linkIcon: "https://littlegreengames.itch.io/only-way-is-up",
-    //     textHeaderIcon: "Website", textMoreIcon: "(free to earn)",
-    // },
     {
-        id: 0, title: "Axie Tri-Force", description: 
-        "First, Little Green Games and SkyMavis have combined forces, and their creation is now available on Greenlight.",
-        image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
-        imageIcon: "./imageWeb/skymavis.png",
-        linkIcon: "https://welcome.skymavis.com/download/",
-        textHeaderIcon: "Mavis Hub", textMoreIcon: "(available)",
+        id: 0, title: "Lucky Blind Bag", description: 
+        "A popular game that combines blind bag opening, and using items to play mini games. Designed to keep players engaged by providing a variety of gameplay experiences.",
+        image: "./kingCat/images/games/blindBag/icon.png",
+        imageIcon: "https://img.icons8.com/?size=100&id=32338&format=png&color=000000",
+        linkIcon: "https://drive.google.com/file/d/1mkPQkDqh_vrUBUpSPrttAl2qU_xZg-iC/view?usp=sharing",
+        textHeaderIcon: "Demo APK", textMoreIcon: "build: 16-12-2024",
+        screenShot: [
+            "https://drive.google.com/uc?export=view&id=1Hmhbyjjv6FlZNz1mwsbi8QM-p9dqg_P_",
+            "./kingCat/images/games/blindBag/icon.png"
+        ],
+        videoLink: "https://www.youtube.com/watch?v=MTboc85t5iM&ab_channel=L%C3%A3ng"
     },
-    {
-        id: 1, title: "Axie Backpack", description: 
-        "Turns are your chance to make moves, shoot, use skills, and engage in combat actions, all packed with excitement.",
-        image: "./crossads/images/com.SkyMavis.AxieBackpack.png",
-        imageIcon: "./imageWeb/skymavis.png",
-        linkIcon: "https://welcome.skymavis.com/download/",
-        textHeaderIcon: "Mavis Hub", textMoreIcon: "(available)",
-    }
 ];
 
-
+// Render HTML with screenshot images in a row
 let renderHtml = function (arr) {
     let html = "";
     arr.map((item, index) => {
         html += `
-        <div class="row boder mt-4" style = "background-color: #50C4ED">
-               <div class="col-lg-3 " style=" padding: 25px">
-                        <div class=" ">
-                            <img style="border-radius: 10px; width: 90%;"
-                                class="img-fluid "
-                                alt=""
-                                src="${item.image}">
+        <div class="row border mt-4" style="background-color:rgb(255, 244, 205); border-radius: 10px">
+            <div class="col-lg-3" style="padding: 25px">
+                <div style="padding-bottom: 25px">
+                    <img style="border-radius: 10px; width: 90%;"
+                        class="img-fluid"
+                        alt=""
+                        src="${item.image}">
+                </div>
+                <div onclick="openLink(this)" data-link="${item.linkIcon}" style="padding-top: 25px">
+                    <div class="btn btn-warning button" style="width: 90%; padding-right: 25px; display: flex; align-items: center;">
+                        <div style="padding: 5px;">
+                            <img src="${item.imageIcon}" alt="" style="width: 50px; height: 50px; padding: 5px;">
+                        </div>
+                        <div style="margin-left: 10px;">
+                            <span style="font-size: 18px; padding: 0px; margin: 0px;">${item.textHeaderIcon}</span>
+                            <p style="font-size: 12px; padding: 0px; margin: 0px;">${item.textMoreIcon}</p>
                         </div>
                     </div>
-                    <div class="col-lg-9">
-                        <div class="row text-left mt-4">
-                            <div class="text-left">
-                                <h3>${item.title}</h3>
-                            </div>
-                            <div>
-                                <p style = "font-size: 16px;">${item.description}</p>
-                            </div>
-                        </div>
-                        <div class="row text-left mt-2" id="listButton">
-                            <div class="col-lg-4" onclick="openLink(this)" data-link="${item.linkIcon}">
-                                <div class="btn btn-info button" style="">
-                                    <div style="padding: 5px;">
-                                        <img src="${item.imageIcon}" alt="" style="width: 50px; height: 50px; padding: 5px">
-                                    </div>
-                                    <div style="margin-left: 20px; margin-top: 5px;">
-                                        <span style="font-size: 18px; padding: 0px;margin: 0px;">${item.textHeaderIcon}</span>
-                                        <p style="font-size: 16px;padding: 0px; margin: 0px;">${item.textMoreIcon}</p>
-                                    </div>
-                                </div>
-                            </div>
+                </div>
+            </div>
+
+            <div class="col-lg-9">
+                <div class="row text-left mt-4" style="padding-right: 20px">
+                    <div class="text-left">
+                        <h3>${item.title}</h3>
+                    </div>
+                    <div>
+                        <p style="font-size: 16px;">${item.description}</p>
+                    </div>
+                </div>
+
+                <!-- Screen Shot Section -->
+                <div class="row mt-4" style="padding-bottom: 30px; padding-right: 20px">
+                    <div style="margin-left: 10px;">
+                        <span style="font-size: 18px; padding: 0px; margin: 0px;">Screen shot</span>
+                    </div>
+                    <div class="col-lg-12 mt-3">
+                        <div style="display: flex; flex-wrap: nowrap; overflow-x: auto;">
+                            ${item.screenShot.map((screenshot) => `
+                                <img src="${screenshot}" alt="Screenshot" style="width: auto; height: 150px; margin-right: 15px; border-radius: 10px;">
+                            `).join('')}
                         </div>
                     </div>
+                </div>
+
+                <!-- Video Player Section -->
+                <div class="row mt-4" style="padding-bottom: 30px; padding-right: 20px">
+                    <div style="margin-left: 10px;">
+                        <span style="font-size: 18px; padding: 0px; margin: 0px;">Video demo</span>
+                    </div>
+                    <div class="col-lg-12">
+                        <video controls style="width: 100%; border-radius: 10px;">
+                            <source src="${item.videoLink}" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
+            </div>
         </div>
-        `
-    })
+        `;
+    });
     $("#content").append(html);
-}
+};
+
+
 renderHtml(data);
 let openLink = function (item) {
     let link = $(item).data("link");
